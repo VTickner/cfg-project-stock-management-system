8a155685f05e7f88dd101460d6a4280098b5b518
VTickner <127106836+VTickner@users.noreply.github.com>
2024-10-13 17:03:15 +0100

    Example data added to tables
    - Parts example data added
    - Products example data added
    - Product parts example data added
    - Suppliers example data added

--- stock_database_setup.sql
+++ stock_database_setup.sql
@@ -65,3 +65,44 @@ CREATE TABLE order_item (
   FOREIGN KEY (order_id) REFERENCES `order`(id),
   FOREIGN KEY (product_id) REFERENCES product(id)
 );
+
+INSERT INTO supplier (supplier_name)
+VALUES
+  ('Crystal Gems Co.'),
+  ('Silver Sparkles Ltd'),
+  ('Bee Charmed');
+
+INSERT INTO part (part_name, supplier_part_id, supplier_id, total_stock, low_stock_alert)
+VALUES
+	('Sterling silver chain', 'SSC-1', 2, 50, 5),
+  ('Sterling silver clasp, 9mm', 'SSCl-5', 2, 30, 10),
+  ('Sterling silver heart charm', 'SSC-1', 3, 5, 1),
+  ('October crystal bead, 6mm', 'CB/10', 1, 10, 4),
+  ('Sterling silver jump rings 4mm', 'SSJR-4', 2, 200, 20);
+
+INSERT INTO part_batch (part_id, supplier_id, quantity, unit_cost, remaining_quantity)
+VALUES
+	(1, 2, 20, 0.05, 20),
+  (2, 2, 10, 1.50, 10),
+  (3, 3, 2, 4.25, 2),
+  (3, 1, 50, 0.03, 45);
+
+INSERT INTO product (id, product_name, price, product_type)
+VALUES
+	('SS-1','Sterling silver heart charm necklace', 21.50, 'necklace'),
+  ('SS-2', 'Sterling silver dainty chain necklace', 17.00, 'necklace'),
+  ('SS-3', 'Sterling silver dainty chain bracelet', 15.50, 'bracelet'),
+  ('Supp-1', 'October crystal beads, 6mm', 2.25, 'supply');
+
+INSERT INTO product_part (product_id, part_id, quantity)
+VALUES
+	('SS-1', 1, 40),
+  ('SS-1', 2, 1),
+  ('SS-1', 3, 1),
+  ('SS-1', 5, 3),
+  ('SS-2', 1, 40),
+  ('SS-2', 2, 1),
+  ('SS-2', 5, 2),
+  ('SS-3', 1, 16),
+  ('SS-3', 2, 1),
+  ('SS-3', 5, 2);
